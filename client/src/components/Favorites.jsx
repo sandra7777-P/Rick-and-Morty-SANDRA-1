e17965d6d33adea83ebe96adc571c69caaec7eff
sandra7777-P <profesenaises@gmail.com>
2023-07-06 15:30:25 -0500

    ahora

--- client/src/components/Favorites.jsx
+++ client/src/components/Favorites.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { connect, useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import style from "../styles/Cards.module.css";
 import Card from "./Card";
 
